918ce6222615f95caee854df92b3ae62906aabbe
tejbho <tejbho305@gmail.com>
2023-12-25 17:02:08 -0800

    ForEachLoop

---
+++ ForEachLoop/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] fruits = { "Apple", "Mango", "Orange" };
+
+        for (int i = 0; i < fruits.length; i++)
+            System.out.println(fruits[i]);
+
+        for (String fruit : fruits)
+            System.out.println(fruit);
+    }
+}
