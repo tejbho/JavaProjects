61739bbe776cb3d32e44ea805ae1a588f9a2a36e
tejbho <tejbho305@gmail.com>
2024-01-06 20:39:57 -0800

    UIControl

---
+++ UIControl/src/com/tejas/Main.java
@@ -0,0 +1,11 @@
+package com.tejas;
+
+public class Main {
+    public static void main(String[] args) {
+        UIControl[] controls = {new TextBox(), new CheckBox()};
+        for (var control: controls) {
+            control.render();
+        }
+    }
+
+}
---
+++ UIControl/src/com/tejas/UIControl.java
@@ -0,0 +1,22 @@
+package  com.tejas;
+
+public abstract class UIControl {
+    boolean isEnabled = true;
+
+//    public UIControl(boolean isEnabled) {
+//        this.isEnabled = isEnabled;
+//    }
+
+    public abstract void render();
+    public void enable() {
+        isEnabled = true;
+    }
+
+    public void disable() {
+        isEnabled = false;
+    }
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+}
