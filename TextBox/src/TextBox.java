2dc11700758268024c4351bc355d52c313c4bb1b
tejbho <tejbho305@gmail.com>
2024-01-06 20:39:07 -0800

    TextBox

---
+++ TextBox/src/TextBox.java
@@ -0,0 +1,11 @@
+public class TextBox {
+    public String text = ""; //Field
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void clear() {
+        text = "";
+    }
+}
